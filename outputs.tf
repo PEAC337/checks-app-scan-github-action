51a2365e544fb1b35b8f6a0100f80bdf4794aa5b
PEAC337 <k.j@peaceoutpost.icu>
2025-10-23 17:03:05 -0500

    commit

---
+++ outputs.tf
@@ -0,0 +1,6 @@
+output "instance_public_ip" {
+  value = "https://u-s-ministry-portal-380943501519.us-west1.run.app/"
+  # The actual value to be outputted
+  description = "The public IP address of the EC2 instance"
+  # Description of what this output represents
+}
